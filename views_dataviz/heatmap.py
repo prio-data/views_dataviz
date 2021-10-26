c447dd9f6a2a1d17f78184446d9d50caf3ea906e
rbjansen <r.b.jansen.uu@gmail.com>
2021-10-26 10:32:16 +0200

    adding heatmap

---
+++ views_dataviz/heatmap.py
@@ -0,0 +1,152 @@
+"""Heatmap plotting functions."""
+
+from typing import List, Tuple, Optional, Any
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def plot_heatmap(
+    s: pd.Series,
+    x: Optional[pd.Series] = None,
+    y: Optional[pd.Series] = None,
+    title: Optional[str] = None,
+    cmap: Any = "viridis",
+    ymin: Optional[float] = None,
+    ymax: Optional[float] = None,
+    tick_values: Optional[List[float]] = None,
+    tick_labels: Optional[List[str]] = None,
+    rotation_xlabels=None,
+    textsize: float = 15,
+    figsize: Tuple[float, float] = (10.0, 10.0),
+    legend: bool = True,
+    boundaries: Optional[List[float]] = None,
+    categorical: bool = False,
+    path: Optional[str] = None,
+) -> None:
+    """Plot a heatmap from a (MultiIndexed) Series.
+
+    Args:
+        s: pd.Series of data to plot.
+        x: Optional pd.Series of time labels. Note that if it's non-integer,
+            this needs to be datetime for it to sort correctly.
+        y: Optional pd.Series of group labels.
+        title: Title to add to figure.
+        cmap: Matplotlib colormap to use.
+        ymin: Minimum to map the data to.
+        ymax: Maximum to map the data to.
+        tick_values: List of selected ticks to show in colorbar.
+        tick_values: List of strings for the selected ticks.
+        rotation_xlabels: Degree of rotation applied to the xlabels.
+        textsize: Base size of text. Title/ticks are hardcoded relatives.
+        figsize: Tuple (width, height) to pass as figure size.
+        legend: Bool determining whether to draw a legend.
+        boundaries: Optional boundaries to apply to colorscale. 
+        categorical: Bool to indicate the data is categorical.
+        path: Optional. Write to path if set.
+    """
+    # Take the index values as default axes of the matrix.
+    try:
+        timevar = x.name if x is not None else s.index.names[0]
+        groupvar = y.name if y is not None else s.index.names[1]
+    except IndexError as e:
+        msg = "Series is not xy MultiIndexed. Try providing x and y."
+        raise Exception(msg) from e
+
+    # Concatenate again.
+    df = pd.DataFrame(s).reset_index()
+    if x is not None and y is None:
+        df = pd.concat([s, x], axis=1).reset_index()
+    if x is None and y is not None:
+        df = pd.concat([s, y], axis=1).reset_index()
+    if x is not None and y is not None:
+        df = pd.concat([s, x, y], axis=1).reset_index()
+
+    # Check for series dtype. Factorize if not numeric.
+    if not pd.api.types.is_numeric_dtype(df[s.name]):
+        warnings.warn("Series does not look numeric. Factorizing.")
+        df[s.name] = pd.factorize(df[s.name], sort=True)[0]
+
+    # Set vmin, vmax.
+    vmin = df[s.name].min() if not ymin else ymin
+    vmax = df[s.name].max() if not ymax else ymax
+
+    # Pivot df.
+    df_matrix = df.pivot(index=groupvar, columns=timevar, values=s.name)
+
+    # Set size.
+    plt.figure(figsize=figsize)
+
+    # Plot.
+    ax = sns.heatmap(
+        df_matrix,
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        linewidths=1,
+        cbar=False,
+    )
+    plt.title(title, loc="left", size=textsize + 5, pad=20)
+
+    # Remove axes labels.
+    ax.set_ylabel("")
+    ax.set_xlabel("")
+
+    plt.yticks(fontsize=textsize - 5, va="center")
+    plt.xticks(fontsize=textsize - 5, rotation=rotation_xlabels, ha="center")
+
+    # Make ax for colorbar and add to canvas.
+    if legend:
+        if categorical:
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.1)
+            cax.axis("off")
+            # Reconstruct color map categorically.
+            # Ticks are the unique values in s or specifically provided ones.
+            ticks = s.unique() if tick_values is None else tick_values
+            colorf = plt.get_cmap(cmap)
+            colors = colorf(np.linspace(0, 1, len(ticks)))
+            # Alter labels in same order if requested.
+            labels = sorted(s.unique()) if tick_labels is None else tick_labels
+            # Make patches according to categorical colors.
+            patches = [
+                mpatches.Patch(facecolor=c, edgecolor=c) for c in colors
+            ]
+            legend = cax.legend(
+                patches,
+                labels,  # Sorted unique maps to pd.factorize.
+                handlelength=0.75,
+                loc="upper left",
+                frameon=False,
+            )
+            for text in legend.get_texts():
+                text.set_ha("left")
+        else:
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.1)
+            # Fill in the colorbar and adjust the ticks.
+            if boundaries is not None:
+                norm = mpcol.BoundaryNorm(boundaries=boundaries, ncolors=256)
+            else:
+                norm = plt.Normalize(vmin=vmin, vmax=vmax)
+            sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+            sm._A = []  # pylint: disable=protected-access
+            cbar = plt.colorbar(
+                sm,
+                cax=cax,
+                ticks=tick_values,
+            )
+            cax.tick_params(labelsize=textsize)
+
+            # Assume ticks are labels if only values are provided.
+            if tick_labels is not None:
+                if tick_values is not None:
+                    cbar.set_ticklabels(tick_labels)
+                else:
+                    raise RuntimeError("Need tick values to match labels to.")
+
+    # Finish up and save.
+    if path is not None:
+        plt.savefig(path, dpi=250, bbox_inches="tight")
+        plt.close()
